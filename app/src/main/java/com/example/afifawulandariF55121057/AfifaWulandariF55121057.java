c6019ae174ae64c84ebfa055463db9c4c3c1e980
USER <afifasantoso@gmail.com>
2022-11-20 20:06:11 +0700

    listview

---
+++ app/src/main/java/com/example/afifawulandariF55121057/AfifaWulandariF55121057.java
@@ -0,0 +1,14 @@
+package com.example.afifawulandariF55121057;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AfifaWulandariF55121057 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_afifa_wulandari_f55121057);
+    }
+}
